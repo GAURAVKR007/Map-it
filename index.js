c192000133998093c1592087166e640f67b37f75
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-04-05 16:37:33 +0530

    Responsiveness added to the login signup form

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
+// const signinDiv = document.querySelector('.sign-in')
 const main = document.getElementById('main');
 
 signUpButton.addEventListener('click', () => {
     main.classList.add("right-panel-active");
+    // signinDiv.style.display = "none";
 });
 
 signInButton.addEventListener('click', () => {
     main.classList.remove("right-panel-active");
+    // signinDiv.style.display = "block";
 });
 
+// signUpButton.addEventListener('click', () => {
+//     main.classList.add("top-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+//     main.classList.remove("top-panel-active");
+// });
 
 
 
